11cfcc7f1c848d8b030e4bfc561966e32f57ab70
emanresu <2board.admin@protonmail.com>
2021-01-25 13:13:18 -0500

    added install and remove scripts

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/sh
+
+cp luksman.py /usr/local/bin/
+cp luksman /usr/local/bin/
+
+echo "Done.\n"
+
+exit 0
---
+++ uninstall.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/sh
+
+rm -f /usr/local/bin/luksman.py
+rm -f /usr/local/bin/luksman
+
+echo "Done.\n"
+
+exit 0
